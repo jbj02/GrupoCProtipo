49485a2fa9893d6892950749362eb6b55ad0bc7f
Bauti Justo <bauti02justo@outlook.com>
2024-05-17 18:57:32 -0300

    Test

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace ProtipoCU2yCU4
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new PreparadorForm());
+            Application.Run(new EstibadorForm());
         }
     }
 }
